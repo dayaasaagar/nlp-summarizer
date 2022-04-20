452632d08db9e7465587ebb6646fda598ef6d729
dayaasaagar <67572290+dayaasaagar@users.noreply.github.com>
2022-04-20 17:36:32 +0530

    Update news_summ.py

--- news_summ.py
+++ news_summ.py
@@ -7,9 +7,9 @@ Created on Sat Apr 16 12:00:37 2022
 
 import streamlit as st 
 from bert_summ import bert_sum
-image = Image.open('maxresdefault.jpg')
+#image = Image.open('maxresdefault.jpg')
 
-st.image(image, caption='123')
+#st.image(image, caption='123')
 #from bert_summ import bert_sum
 st.title('News text summarizer')
 
